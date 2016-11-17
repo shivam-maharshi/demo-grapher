c55f0f59ca62a2fd8477d9373d4be59ea616bc6c
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-16 20:23:12 -0500

    Adding a few initial stuff for filters.

--- src/views.py
+++ src/views.py
@@ -1,7 +1,16 @@
 from src import app
+from flask import send_from_directory
 from flask.templating import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', message='Home Page for DemoGrapher')
+    return render_template('index.html', message='DemoGrapher is up and running!')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+@app.route('/assets/<path:path>')
+def send_js(path):
+    return send_from_directory('assets/', path)
