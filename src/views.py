eebc647540636ea43280b3ac5bc3d69a23f933bd
shivam-maharshi <shivam.maharshi@gmail.com>
2016-11-22 01:30:21 -0500

    Created first layout for the filters and basic JS event handling.

--- src/views.py
+++ src/views.py
@@ -1,6 +1,9 @@
-from src import app
+import os
+
 from flask import send_from_directory
 from flask.templating import render_template
+
+from src import app
 
 @app.route('/')
 @app.route('/index')
@@ -11,6 +14,10 @@ def index():
 def view():
     return render_template('view.html')
 
-@app.route('/assets/<path:path>')
-def send_js(path):
-    return send_from_directory('assets/', path)
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+@app.route('/static/<path:path>')
+def render_static_assets(path):
+    return send_from_directory(os.getcwd() + '/static/', path)
